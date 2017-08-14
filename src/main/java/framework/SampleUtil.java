de88d10e774c71611130fce0ad2f6cf823290faf
Tom <tom_p@experitest.com>
2017-08-14 15:31:50 +0300

    added some files to src

---
+++ src/main/java/framework/SampleUtil.java
@@ -0,0 +1,8 @@
+package framework;
+
+/**
+ * appium-demo
+ * Created by tom.ben-simhon on 8/14/2017.
+ */
+public class SampleUtil {
+}
---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,6 @@
+/**
+ * appium-demo
+ * Created by tom.ben-simhon on 8/14/2017.
+ */
+public class SampleTest {
+}
